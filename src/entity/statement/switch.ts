8d7c09f267557b20c7f8c27ce30423be604af08e
Cp-John <e0376984@u.nus.edu>
2023-03-15 16:45:20 +0800

    Supported default in switch statement

--- src/entity/statement/switch.ts
+++ src/entity/statement/switch.ts
@@ -27,12 +27,18 @@ class Case {
 
 export class Switch extends Statement {
   private expression: Expression
+  private defaultExecutables: (Statement | Block)[]
   private body: (Case | Statement | Block)[]
 
-  constructor(expression: Expression, body: (Case | Statement | Block)[]) {
+  constructor(
+    expression: Expression,
+    body: (Case | Statement | Block)[],
+    defaultExecutables: (Statement | Block)[]
+  ) {
     super()
     this.expression = expression
     this.body = body
+    this.defaultExecutables = defaultExecutables
   }
 
   static parse(
@@ -48,22 +54,39 @@ export class Switch extends Statement {
     lexer.eatDelimiter(')')
     lexer.eatDelimiter('{')
     const body = []
+    let isInDefault = false
+    const defaultExecutables: (Statement | Block)[] = []
     while (!lexer.matchDelimiter('}')) {
+      if (lexer.matchKeyword('default')) {
+        if (isInDefault) {
+          throw new Error(lexer.formatError('multiple default labels in one switch'))
+        }
+        lexer.eatKeyword('default')
+        lexer.eatDelimiter(':')
+        isInDefault = true
+      }
       while (lexer.matchKeyword('case')) {
         body.push(Case.parse(env, lexer))
       }
       if (lexer.matchDelimiter('{')) {
-        body.push(Block.parse(env, lexer, true, false, returnType))
+        const block = Block.parse(env, lexer, true, false, returnType)
+        body.push(block)
+        if (isInDefault) {
+          defaultExecutables.push(block)
+        }
       } else if (lexer.matchDataType()) {
         throw new Error(lexer.formatError('expected an expression'))
       } else {
-        Statement.parse(env, lexer, true, false, returnType).forEach(statement =>
+        Statement.parse(env, lexer, true, false, returnType).forEach(statement => {
           body.push(statement)
-        )
+          if (isInDefault) {
+            defaultExecutables.push(statement)
+          }
+        })
       }
     }
     lexer.eatDelimiter('}')
-    return [new Switch(expression, body)]
+    return [new Switch(expression, body, defaultExecutables)]
   }
 
   execute(env: Frame, rts: Frame[], context: any): void {
@@ -81,11 +104,14 @@ export class Switch extends Statement {
         }
       } catch (err: any) {
         if (err == 'BREAK') {
-          break
+          return
         } else {
           throw err
         }
       }
+    }
+    if (!matched) {
+      this.defaultExecutables.forEach(executable => executable.execute(env, rts, context))
     }
   }
 }
